c703eeb453d745243086bb4494c7a24a94f894cc
rootlocus22 <rahuldubey220890@gmail.com>
2025-12-19 23:38:20 +0530

    fix seo broken links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/topic/:slug',
+        destination: '/learn/:slug',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
