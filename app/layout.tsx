abe6d44ff6f1db0d903b29880cca1ba6ff6f9365
rootlocus22 <rahuldubey220890@gmail.com>
2025-12-24 00:40:23 +0530

    cross sell english gyani

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 import PaywallModal from "@/components/PaywallModal";
 import AnalyticsTracker from "@/components/AnalyticsTracker";
 import { Analytics } from "@vercel/analytics/next";
+import MicrosoftClarity from "@/components/MicrosoftClarity";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -136,16 +137,7 @@ export default function RootLayout({
             gtag('config', 'AW-17492039594');
           `}
         </Script>
-        {/* Microsoft Clarity */}
-        <Script id="clarity-script" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "p03xhgw5cr");
-          `}
-        </Script>
+        <MicrosoftClarity />
         <AnalyticsTracker />
         {children}
         <PaywallModal />
